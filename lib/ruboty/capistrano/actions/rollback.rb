2738e72c681c3933ba12e86d139be4601bbac6b0
Morioka Shuhei <selmertsx@gmail.com>
2016-07-22 20:03:59 +0900

    add rollback script

---
+++ lib/ruboty/capistrano/actions/rollback.rb
@@ -0,0 +1,39 @@
+module Ruboty
+  module Capistrano
+    module Actions
+      class Rollback < Ruboty::Actions::Base
+        class RollbackError < StandardError; end
+
+        attr_reader :message, :path, :env, :role
+
+        def initialize(message)
+          @message = message
+          @env = Ruboty::Capistrano.config.env
+          @role = message.match_data[1]
+          @path = Ruboty::Capistrano.config.repository_path[@role]
+        end
+
+        def call
+          message.reply("#{@env}環境の#{@role}をrollbackします")
+          rollback
+          message.reply("#{@env}環境の#{@role}をrollback完了しました")
+        rescue => e
+          err_message = <<~TEXT
+            :cop:問題が発生しました:cop:
+            ```
+            #{e.message}
+            ```
+          TEXT
+          message.reply(err_message)
+        end
+
+        private
+        def deploy
+          cmd = "cd #{path} && bundle && bundle exec cap #{@env} deploy:rollback"
+          out, err, status = Bundler.with_clean_env { Open3.capture3(cmd) }
+          raise RollbackError.new(err) unless err.empty?
+        end
+      end
+    end
+  end
+end
--- lib/ruboty/handlers/capistrano.rb
+++ lib/ruboty/handlers/capistrano.rb
@@ -6,9 +6,14 @@ module Ruboty
       env :DEPLOY_REPOSITORY_PATH, 'Deploy用の Repository Path を設定する'
 
       on(/deploy\s+(.*)\s+(.*)/m, name: 'deploy', description: 'deployする')
+      on(/rollback\s+(.*)/m, name: 'rollback', description: 'rollbackする')
 
       def deploy(message)
         Ruboty::Capistrano::Actions::Deploy.new(message).call
+      end
+
+      def rollback(message)
+        Ruboty::Capistrano::Actions::Rollback.new(message).call
       end
     end
   end
