a4d2fcbef33fd6a012e350d4c410099daee0f638
Takumi Kaji <ivezuki3@gmail.com>
2016-12-07 21:40:01 +0900

    Verify exception before deploy

--- lib/ruboty/capistrano/actions/deploy.rb
+++ lib/ruboty/capistrano/actions/deploy.rb
@@ -1,32 +1,36 @@
-require 'ruboty/capistrano/github'
+require 'ruboty/capistrano/verification'
+require 'ruboty/capistrano/deploy_source'
 
 module Ruboty
   module Capistrano
     module Actions
       class Deploy < Ruboty::Actions::Base
         class DeployError < StandardError; end
-        class InvalidDeploySettingError; end
-        class NoBranchError < StandardError; end
 
-        attr_reader :message, :path, :name, :env, :branch, :role
+        attr_reader :message, :path, :repo, :env, :branch, :role
 
         def initialize(message)
           @message = message
           @env = Ruboty::Capistrano.config.env
           @role = message.match_data[1]
           @path = Ruboty::Capistrano.config.repository_path[@role]
-          @name = Ruboty::Capistrano.config.repository_name[@role]
+          @repo = Ruboty::Capistrano.config.repository_name[@role]
           @branch = message.match_data[2] || ENV['DEFAULT_BRANCH']
           @logger = Logger.new(deploy_log_path)
         end
 
         def call
           message.reply("#{@env}環境の#{@role}にBRANCH:#{@branch}をdeployします")
+          verify
           deploy
           message.reply("#{@env}環境の#{@role}にBRANCH:#{@branch}をdeploy完了しました")
-        rescue NoBranchError => e
+        rescue Ruboty::Capistrano::Verification::NoBranchError => e
           err_message = <<~TEXT
             :u7121:#{e.message}:u7121:
+          TEXT
+        rescue Ruboty::Capistrano::Verification::InvalidDeploySettingError => e
+          err_message = <<~TEXT
+           :no_entry_sign:#{e.message}:no_entry_sign:
           TEXT
         rescue => e
           err_message = <<~TEXT
@@ -38,15 +42,18 @@ module Ruboty
         end
 
         private
+
+        def verify
+          verification = Ruboty::Capistrano::Verification.new(
+            env: env,
+            role: role,
+            deploy_source: Ruboty::Capistrano::DeploySource.new(repo, branch)
+          )
+          verification.prod_branch_limit
+          verification.exist_branch_check
+        end
+
         def deploy
-          if @env == 'production' && @branch != 'master'
-            raise InvalidDeploySettingError.new('production環境はmaster以外でdeploy出来ません')
-          end
-
-          unless Ruboty::Capistrano::Github.branch_exist?(name, branch)
-            raise NoBranchError, "#{role}のリポジトリに#{branch}ブランチは存在しません"
-          end
-
           cmd = "cd #{path} && bundle && bundle exec cap #{@env} deploy BRANCH=#{@branch}"
           out, err, status = Bundler.with_clean_env { Open3.capture3(cmd) }
           @logger.info out
