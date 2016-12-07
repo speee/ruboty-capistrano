087b871ad7b2b1e330ea69c37eceec94b105a96e
Takumi Kaji <ivezuki3@gmail.com>
2016-12-07 13:52:12 +0900

    Add Ruboty::Capistrano.config spec

--- spec/ruboty/capistrano_spec.rb
+++ spec/ruboty/capistrano_spec.rb
@@ -8,4 +8,30 @@ describe Ruboty::Capistrano do
   it 'does something useful' do
     expect(false).to eq(true)
   end
+
+  describe '.config' do
+    let(:expected_repo_name) do
+      {
+        'sample' => 'hoge/sample-repo',
+        'admin' => 'hoge/admin-repo'
+      }
+    end
+    let(:expected_repo_path) do
+      {
+        'sample' => '/path/to/sample',
+        'admin' => '/path/to/admin'
+      }
+    end
+
+    before do
+      allow(ENV).to receive(:[]).and_call_original
+      allow(ENV).to receive(:[]).with('DEPLOY_REPOSITORY_NAME').and_return('sample:hoge/sample-repo;admin:hoge/admin-repo')
+      allow(ENV).to receive(:[]).with('DEPLOY_REPOSITORY_PATH').and_return('sample:/path/to/sample;admin:/path/to/admin')
+    end
+
+    it do
+      expect(Ruboty::Capistrano.config.repository_name).to eq(expected_repo_name)
+      expect(Ruboty::Capistrano.config.repository_path).to eq(expected_repo_path)
+    end
+  end
 end
