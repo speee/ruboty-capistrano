5ffe036bded9f2791398832f665426d50d37fb20
Takumi Kaji <ivezuki3@gmail.com>
2016-12-14 14:23:16 +0900

    Refactor

--- lib/ruboty/capistrano/deploy_source.rb
+++ lib/ruboty/capistrano/deploy_source.rb
@@ -1,24 +1,26 @@
 require 'octokit'
 
-module Ruboty::Capistrano
-  class DeploySource
-    attr_reader :repo, :branch
+module Ruboty
+  module Capistrano
+    class DeploySource
+      attr_reader :repo, :branch
 
-    def initialize(repo:, branch:)
-      @repo = repo
-      @branch = branch
-    end
+      def initialize(repo:, branch:)
+        @repo = repo
+        @branch = branch
+      end
 
-    def exist_github?
-      !!octokit_client.branch(repo, branch)
-    rescue Octokit::NotFound
-      false
-    end
+      def exist_github?
+        !!octokit_client.branch(repo, branch)
+      rescue Octokit::NotFound
+        false
+      end
 
-    private
+      private
 
-    def octokit_client
-      @octokit_client ||= Octokit::Client.new(access_token: Ruboty::Capistrano.config.github_access_token)
+      def octokit_client
+        @octokit_client ||= Octokit::Client.new(access_token: Ruboty::Capistrano.config.github_access_token)
+      end
     end
   end
 end
