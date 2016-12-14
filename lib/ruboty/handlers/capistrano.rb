2af88438e2870b07fe36a1986c5e83892320aac5
Morioka Shuhei <selmertsx@gmail.com>
2016-12-14 19:59:46 +0900

    refactoring deployment class

--- lib/ruboty/handlers/capistrano.rb
+++ lib/ruboty/handlers/capistrano.rb
@@ -1,7 +1,6 @@
-require 'ruboty/capistrano/actions/deploy'
+require 'ruboty/capistrano/actions/deployment'
 require 'ruboty/capistrano/actions/rollback'
 require 'ruboty/capistrano/verification'
-require 'ruboty/capistrano/deploy_source'
 
 module Ruboty
   module Handlers
@@ -12,31 +11,25 @@ module Ruboty
       on(/rollback\s+(.*)/m, name: 'rollback', description: 'rollbackする')
 
       def deploy(message)
-        config = Ruboty::Capistrano.config
-        role, branch = message.match_data[1..2]
-        env = config.env
-
-        Ruboty::Capistrano::Verification.new(
-          env: env,
-          deploy_source: Ruboty::Capistrano::DeploySource.new(
-            repo: config.remote_repo_path[role],
-            branch: branch
-          )
-        ).execute
-
+        deployment = Actions::Deployment.new(deploy_params(message))
         message.reply("#{env}環境の#{role}にBRANCH:#{branch}をdeployします")
-        Ruboty::Capistrano::Actions::Deploy.new(
-          env: env,
-          repo_path: config.local_repo_path[role],
-          branch: branch,
-        ).call
-        message.reply("#{env}環境の#{role}にBRANCH:#{branch}をdeploy完了しました")
-      rescue => e
-        message.reply(e.message)
+
+        if deployment.run
+          message.reply('deployを完了しました')
+        else
+          message.reply(deployment.errors.join(','))
+        end
       end
 
       def rollback(message)
         Ruboty::Capistrano::Actions::Rollback.new(message).call
+      end
+
+      private
+
+      def deploy_params(message)
+        role, branch = message.match_data[1..2]
+        { role: role, branch: branch }
       end
     end
   end
